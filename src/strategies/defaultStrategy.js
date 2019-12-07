108888222c09cec72a90d5b74a8e1ff537567281
nullablebool <kurt.gooding@gmail.com>
2019-12-06 17:57:37 -0800

    Favour BroadcastChannelStrategy for default
    The readme states that LocalStorageStrategy is the fallback however the code does not reflect this.

--- src/strategies/defaultStrategy.js
+++ src/strategies/defaultStrategy.js
@@ -2,14 +2,15 @@ import BroadcastChannelStrategy from "./broadcastChannel";
 import LocalStorageStrategy from "./localStorage";
 
 export default function createDefaultStrategy() {
-  /* istanbul ignore next: browser-dependent code */
-  if (LocalStorageStrategy.available()) {
-    return new LocalStorageStrategy();
-  }
 
   /* istanbul ignore next: browser-dependent code */
   if (BroadcastChannelStrategy.available()) {
     return new BroadcastChannelStrategy();
+  }
+  
+  /* istanbul ignore next: browser-dependent code */
+  if (LocalStorageStrategy.available()) {
+    return new LocalStorageStrategy();
   }
 
   /* istanbul ignore next: browser-dependent code */
